1e02c19d2d1d021245b35baf4d362cdb1285de0b
Harsh Patel <patelharsh9797@gmail.com>
2024-12-01 06:26:16 +0000

    Added serizlizer and fetch the data from the db

--- src/advocate/views.py
+++ src/advocate/views.py
@@ -1,10 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Advocate
+from .serializers import AdvocateSerializer
+
+
 @api_view(["GET"])
 def advocate(request):
-    data = ["harsh", "Dennis"]
-    return Response(data)
+    advocates = Advocate.objects.all()
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data)
 
 @api_view(["GET"])
 def advocate_detail(request,username):
