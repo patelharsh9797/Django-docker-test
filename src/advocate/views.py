3c6eed18cd1a52a00a888f6ddcbbde52a1fa2d22
Harsh Patel <patelharsh9797@gmail.com>
2024-12-01 07:08:50 +0000

    Basic CRUD done

--- src/advocate/views.py
+++ src/advocate/views.py
@@ -2,16 +2,56 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .models import Advocate
+from django.db.models import Q
 from .serializers import AdvocateSerializer
 
 
-@api_view(["GET"])
-def advocate(request):
-    advocates = Advocate.objects.all()
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(serializer.data)
 
-@api_view(["GET"])
+# GET /advocates
+# POST /advocates
+
+# GET /advocates/:id
+# PUT /advocates/:id
+# DELETE /advocates/:id
+
+
+@api_view(["GET", "POST"])
+def advocate(request):
+    
+    if request.method == "GET":
+        query = request.GET.get("query")
+
+        if query == None:
+            query = ""
+        
+        advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
+        serializer = AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data)
+
+    if request.method == "POST":
+        advocates = Advocate.objects.create(
+            username=request.data["username"],
+            bio=request.data["bio"],
+        )
+        serializer = AdvocateSerializer(advocates, many=False)
+        return Response(serializer.data)
+
+
+@api_view(["GET", "PUT", "DELETE"])
 def advocate_detail(request,username):
-    data = {"username" : username}
-    return Response(data)
+    advocate = Advocate.objects.get(username=username)
+
+    if request.method == "GET":
+        serializer = AdvocateSerializer(advocate, many=False)
+        return Response(serializer.data)
+
+    if request.method == "PUT":
+        advocate.username = request.data["username"]
+        advocate.bio = request.data["bio"]
+        advocate.save()
+        serializer = AdvocateSerializer(advocate, many=False)
+        return Response(serializer.data)
+
+    if request.method == "DELETE":
+        advocate.delete()
+        return Response("Advocate deleted")
